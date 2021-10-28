5ea819ed9dec19cd46d5bd8981d2995de5c56847
gidwikintan <gidwikintan@gmail.com>
2021-10-28 23:53:53 +0700

    create budget planner layouting

---
+++ src/components/ExpenseTotal.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ExpenseTotal = () => {
+    return (
+        <>
+        <div className="alert alert-secondary">
+            <span>Spent so far : $1200</span>
+        </div>
+            </>
+     );
+}
+ 
+export default ExpenseTotal;
