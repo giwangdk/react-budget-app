34b420d74f76d771231d5cf6b97e2e371a4479f4
gidwikintan <gidwikintan@gmail.com>
2021-10-31 22:53:58 +0700

    add new expenses form

--- src/components/AddExpenseForm.js
+++ src/components/AddExpenseForm.js
@@ -1,16 +1,38 @@
-import React from 'react'
+import React, {useState, useContext} from 'react'
+import {AppContext} from '../context/AppContext';
+import { v4 as uuidv4 } from 'uuid';
 
 const AddExpenseForm = () => {
+    const { dispatch } = useContext(AppContext);
+
+    const [name, setName] = useState('')
+    const [cost, setCost] = useState('')
+
+    const onSubmit = (event) => {
+        event.preventDefault()
+
+        const expense = {
+            id: uuidv4(),
+            name:name,
+            cost: parseInt(cost)
+        }
+
+        dispatch({
+            type: 'ADD_EXPENSE',
+            payload:expense
+        })
+    }
+
     return (
-        <form action="">
+        <form onSubmit={onSubmit}>
             <div className="row">
                 <div className="col-sm">
                     <label htmlFor="name">Name</label>
-                    <input type="text" className="form-control" required="required" id='name' />
+                    <input type="text" className="form-control" required="required" id='name' value={name} onChange={(event)=>setName(event.target.value)} />
                 </div>
                 <div className="col-sm">
                     <label htmlFor="cost">Cost</label>
-                    <input type="text" className="form-control" required="required" id='cost' />
+                    <input type="text" className="form-control" required="required" id='cost' value={cost} onChange={(event) => setCost(event.target.value)} />
                 </div>
                 <div className="col-sm">
                     <button className="btn btn-primary" type="submit">save</button>
