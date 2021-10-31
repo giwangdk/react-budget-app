b6861540f4191a3dd14c61e89bf4d9f4bf03037b
gidwikintan <gidwikintan@gmail.com>
2021-10-31 21:18:07 +0700

    use context for budget and expenses

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,14 +1,10 @@
-import React from 'react';
+import React, {useContext} from 'react'
+import { AppContext } from '../context/AppContext';
 import ExpenseItem from './ExpenseItem';
 
 const ExpenseList = () => {
-    const expenses = [
-        {id:1231232, name:'Shopping', cost:50},
-        {id:1231232, name:'Holiday', cost:50},
-        {id:1231232, name:'Transportation', cost:50},
-        {id:1231232, name:'Fuel', cost:50},
-        {id:1231232, name:'Child Care', cost:50},
-    ]
+    const { expenses } = useContext(AppContext)
+    
     return (
         <ul className="list-group">
             {expenses.map((expense)=>(
